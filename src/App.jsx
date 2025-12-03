9c0dcc57d5a0f48c7b6a76f0edc36386b2b2341d
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-03 22:07:46 +1100

    Refactor file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import General from './General.jsx'
 import Education from './Education.jsx'
@@ -12,6 +11,7 @@ function App() {
     <>
       <div>
         <General />
+        <br />
         <Education />
        
       </div>
