5cbba01ea364f98f84d91b33d43562443a55f180
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-07 15:47:09 +1100

    Refactor similar functions to accept key as arg

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,333 +7,263 @@
 //submit - hide inputs, add p element with value, add edit & delete button
 
 import { useState } from "react";
-import  '../styles/Experience.css';
-
-
+import "../styles/Experience.css";
 
 export default function Experience() {
-    const blankExperience = {workplace: '', position:'', responsibilities: [], from:'', to: ''}
-    // const [expId, setExpId] = useState(0)
-    const [experience, setExperience] = useState(blankExperience)
-    
-    const [experienceArray, setExperienceArray] =useState([])
-    const [activeIndex, setActiveIndex] = useState(0)
+  const blankExperience = {
+    workplace: "",
+    position: "",
+    responsibilities: [],
+    from: "",
+    to: "",
+  };
 
-  
-    
+  const [experience, setExperience] = useState(blankExperience);
 
-    
+  const [experienceArray, setExperienceArray] = useState([]);
 
-    function handleWorkplaceChange(e){
-        const newExperience = {...experience, workplace: (e.target.value)}
-        setExperience(newExperience)
-    }
 
-     function handlePositionChange(e){
-        const newExperience = {...experience, position: (e.target.value)}
-         setExperience(newExperience)
-    }
+  function handleChange(e, param) {
+    const newExperience = { ...experience, [param]: e.target.value };
+    setExperience(newExperience);
+  }
 
-     function handleResponsibilitiesChange(e){
-        const newExperience = {...experience, responsibilities: (e.target.value)}
-         setExperience(newExperience)
-    }
+//   function handlePositionChange(e) {
+//     const newExperience = { ...experience, position: e.target.value };
+//     setExperience(newExperience);
+//   }
 
-     function handleStartDateChange(e){
-        const newExperience = {...experience, from: (e.target.value)}
-        setExperience(newExperience)
-    }
+//   function handleResponsibilitiesChange(e) {
+//     const newExperience = { ...experience, responsibilities: e.target.value };
+//     setExperience(newExperience);
+//   }
 
-     function handleEndDateChange(e){
-        const newExperience = {...experience, to: (e.target.value)}
-        setExperience(newExperience)
-    }
+//   function handleStartDateChange(e) {
+//     const newExperience = { ...experience, from: e.target.value };
+//     setExperience(newExperience);
+//   }
 
 
 
+  function addExperience() {
+    const newExperience = { ...experience, id: crypto.randomUUID() };
+    setExperienceArray([...experienceArray, newExperience]);
+    setExperience(blankExperience);
+  }
 
-    function addExperience(){
-       
-       const newExperience = {...experience, id:crypto.randomUUID() }
-        setExperienceArray([...experienceArray, newExperience])
-        setExperience(blankExperience)
-        const parentDiv = document.activeElement.parentElement;
-        // const infoRow = parentDiv.querySelector('.info-row')
-        // infoRow.classList.remove('hidden')
-        // const inputRow = parentDiv.querySelector('.details-input-div')
-        // infoRow.classList.add('hidden')
+  function handleDeleteClick(id) {
+    const newArray = [...experienceArray];
+    const itemIndex = newArray.findIndex((item) => item.id === id);
+    const itemToEdit = newArray.find((exp, index) => index === itemIndex);
+    console.log(itemToEdit);
+    newArray.splice(itemIndex, 1);
+    setExperienceArray(newArray);
+    setExperience(blankExperience);
+  }
 
-    }
+//   function handleWorkplaceEdit(e, id) {
+//     const newArray = [...experienceArray];
+//     const itemIndex = newArray.findIndex((item) => item.id === id);
+//     const itemToEdit = newArray.find((exp, index) => index === itemIndex);
+//     console.log(itemToEdit);
+//     const newItem = { ...itemToEdit, workplace: e.target.value };
+//     newArray.splice(itemIndex, 1, newItem);
+//     setExperienceArray(newArray);
+//   }
 
-    function handleDeleteClick(id){
- 
-        const newArray = [...experienceArray]
-        const itemIndex = newArray.findIndex(item=> item.id===id)
-        const itemToEdit = (newArray.find((exp, index) => index === itemIndex))
-        console.log(itemToEdit)
-        
-        newArray.splice(itemIndex, 1 )
-        setExperienceArray(newArray)
-        
-        // experienceArray.splice(itemIndex, 0)
-        // experienceArray.map(exp =>
-        //     console.log(exp)
-        // )
-        setExperienceArray(newArray)
-        setExperience(blankExperience)
- 
-        
-    }
+  function handleEdit(e, id, param) {
+    const newArray = [...experienceArray];
+    const itemIndex = newArray.findIndex((item) => item.id === id);
+    const itemToEdit = newArray.find((exp, index) => index === itemIndex);
+    console.log(itemToEdit);
+    const newItem = { ...itemToEdit, [param]: e.target.value };
+    newArray.splice(itemIndex, 1, newItem);
+    setExperienceArray(newArray);
+  }
 
+//   function handlePositionEdit(e, id) {
+//     const newArray = [...experienceArray];
+//     const itemIndex = newArray.findIndex((item) => item.id === id);
+//     const itemToEdit = newArray.find((exp, index) => index === itemIndex);
+//     console.log(itemToEdit);
+//     const newItem = { ...itemToEdit, position: e.target.value };
+//     newArray.splice(itemIndex, 1, newItem);
+//     setExperienceArray(newArray);
+//   }
 
-    function handleWorkplaceEdit(e, id){
-        const newArray = [...experienceArray]
-        const itemIndex = newArray.findIndex(item=> item.id===id)
-        const itemToEdit = (newArray.find((exp, index) => index === itemIndex))
-        console.log(itemToEdit)
-        const newItem = {...itemToEdit, workplace: (e.target.value)}
-        newArray.splice(itemIndex, 1, newItem )
-        setExperienceArray(newArray)
-        
-      
-    }
+//   function handleResponsibilitiesEdit(e, id) {
+//     const newArray = [...experienceArray];
+//     const itemIndex = newArray.findIndex((item) => item.id === id);
+//     const itemToEdit = newArray.find((exp, index) => index === itemIndex);
+//     console.log(itemToEdit);
+//     const newItem = { ...itemToEdit, responsibilities: e.target.value };
+//     newArray.splice(itemIndex, 1, newItem);
+//     setExperienceArray(newArray);
+//   }
 
-     function handleEdit(e, id, param){
-        const newArray = [...experienceArray]
-        const itemIndex = newArray.findIndex(item=> item.id===id)
-        const itemToEdit = (newArray.find((exp, index) => index === itemIndex))
-        console.log(itemToEdit)
-        const newItem = {...itemToEdit, param: (e.target.value)}
-        newArray.splice(itemIndex, 1, newItem )
-        setExperienceArray(newArray)
-        
-      
-    }
+//   function handleStartDateEdit(e, id) {
+//     const newArray = [...experienceArray];
+//     const itemIndex = newArray.findIndex((item) => item.id === id);
+//     const itemToEdit = newArray.find((exp, index) => index === itemIndex);
+//     console.log(itemToEdit);
+//     const newItem = { ...itemToEdit, from: e.target.value };
+//     newArray.splice(itemIndex, 1, newItem);
+//     setExperienceArray(newArray);
+//   }
 
+//   function handleEndDateEdit(e, id) {
+//     const newArray = [...experienceArray];
+//     const itemIndex = newArray.findIndex((item) => item.id === id);
+//     const itemToEdit = newArray.find((exp, index) => index === itemIndex);
+//     console.log(itemToEdit);
+//     const newItem = { ...itemToEdit, to: e.target.value };
+//     newArray.splice(itemIndex, 1, newItem);
+//     setExperienceArray(newArray);
+//   }
 
-    function handlePositionEdit(e, id){
-        const newArray = [...experienceArray]
-        const itemIndex = newArray.findIndex(item=> item.id===id)
-        const itemToEdit = (newArray.find((exp, index) => index === itemIndex))
-        console.log(itemToEdit)
-        const newItem = {...itemToEdit, position: (e.target.value)}
-        newArray.splice(itemIndex, 1, newItem )
-        setExperienceArray(newArray)
-        
-      
-    }
+  return (
+    <div>
+      {experienceArray.length > 0 ? <h3>Experience</h3> : null}
 
-    function handleResponsibilitiesEdit(e, id){
-        const newArray = [...experienceArray]
-        const itemIndex = newArray.findIndex(item=> item.id===id)
-        const itemToEdit = (newArray.find((exp, index) => index === itemIndex))
-        console.log(itemToEdit)
-        const newItem = {...itemToEdit, responsibilities: (e.target.value)}
-        newArray.splice(itemIndex, 1, newItem )
-        setExperienceArray(newArray)
-        
-      
-    }
-
-    function handleStartDateEdit(e, id){
-        const newArray = [...experienceArray]
-        const itemIndex = newArray.findIndex(item=> item.id===id)
-        const itemToEdit = (newArray.find((exp, index) => index === itemIndex))
-        console.log(itemToEdit)
-        const newItem = {...itemToEdit, param: (e.target.value)}
-        newArray.splice(itemIndex, 1, newItem )
-        setExperienceArray(newArray)
-        
-      
-    }
-
-     function handleEndDateEdit(e, id){
-        const newArray = [...experienceArray]
-        const itemIndex = newArray.findIndex(item=> item.id===id)
-        const itemToEdit = (newArray.find((exp, index) => index === itemIndex))
-        console.log(itemToEdit)
-        const newItem = {...itemToEdit, to: (e.target.value)}
-        newArray.splice(itemIndex, 1, newItem )
-        setExperienceArray(newArray)
-        
-      
-    }
-    
-   
-        return(
-             
-            <div>
-            {experienceArray.length>0 ? <h3>Experience</h3> : null }
-
-            
-             <div className="experience-frame">
-                
-            {experienceArray.length>0? (
-                experienceArray.map(exp => {
-                return (
-                  
-                        <div className="inputs" key={(exp.id)}>
-                           
-
-            <Input name={'Workplace' + ((experienceArray.indexOf(exp))+1)} type= "text" label={'Workplace' + ' '+((experienceArray.indexOf(exp))+1)}  value={exp.workplace} key={exp.id} onChange={e => handleEdit(e, exp.id)} />{' '}
-
-            <Input name="position" label="Position title" value = {exp.position} onChange={e =>handlePositionEdit(e,exp.id)}/>{' '}
-            <Input label="responsibilities" placeholder = "Enter responsibilities of your role" value = {exp.completed}  onChange={e =>handleResponsibilitiesEdit}/>{' '}
-            <button onClick = {()=>handleDeleteClick(exp.id)}>x</button>
-
-
-
-                         {/* <div className="completed-inputs">
-                {experienceArray
-                .map((exp, i) =>{
-                    
-                   
-                   <div>
-                        <Input name="workplace" type= "text" label="Company name" placeholder="Enter company" value={exp.workplace} onChange={handleWorkplaceChange} />
-
-            <Input name="position" label="Position title" value = {exp.position} onChange={handlePositionChange} />
-            <textarea  label="responsibilities" placeholder = "Enter responsibilities of your role" value = {exp.responsibilities} onChange={handleResponsibilitiesChange} />
-                    </div>
-                })}
-
-            </div> */}
-</div> 
-
-
-
-                    
-                )
-            }) ) : null}
-            </div>
-           <div>
-                     <div className="inputs">
-
-            <Input name="workplace" type= "text" label="Company name" placeholder="Enter company" value={experience.workplace} onChange={handleWorkplaceChange} />{' '}
-
-            <Input name="position" label="Position title" value = {experience.position} onChange={handlePositionChange} />{' '}
-            <Input  label="Responsibilities" placeholder = "Enter responsibilities of your role" value = {experience.completed} onChange={handleResponsibilitiesChange} />{' '}
-             <Input name="start-date" type="date" label="From" value={experience.from} onChange={handleStartDateChange} />
-<           Input name="start-date" type="date" label="To" value={experience.to} onChange={handleStartDateChange} />
-            <button onClick={(addExperience)}>Add Experience</button></div> 
-            </div>
-            <div className="inputs">
-               
-
-            </div>
-             {/* <div>Schools: {schools.map(s =>(
-                <p key={s.school}>{s.school} {s.id}</p>
-            ))}</div>  */}
-            </div>
-        )
-
-
-}
-
-
-
-function Input({label, placeholder, value, onChange, element,  type, idx}){
-
-    return (
-        <>
-    <div className="details-input-div" key={idx}>
-        <label htmlFor = {name}>{label}{': '}
-    
-            <input key = {idx} type ={type} name={label} placeholder={placeholder} value={value} onChange={onChange}  />
-            </label>
-       </div>
-       {/* <div className="info-row">
-          
-            <div className="info-cell">
-            <h4>{label}</h4>
-            <p>{value}</p>
-            </div>
-            <button className="delete-button" onClick={onClick}>📝</button>
-            </div> */}
-    
-       </>
-       
-    )
-}
-
-function InputRow(){
-    return( 
-    <>
-    <div className = "input-row">
-        <Input />
-
-
-
-
-    </div>
-    
-    
-    
-    </>
-    )
-}
-
-
-
-
-// function ExperienceRow({value, label}){
-//     return(
-//         <div className="info-row">
-          
-//             <div className="info-cell">
-//             <h4>{label}</h4>
-//             <p>{value}</p>
-//             </div>
-//             <div className="info-cell">
-//             <h5>Job title: </h5>
-//             <p>{job}</p>
-//             </div>
-//             <div className="info-cell responsibilities">
-//             <h5>Responsibilities: </h5>
-//             <p>{responsibilities}</p>
-//             </div>
-//             <button className="delete-button" onClick={onClick}>📝</button>
-            
-//         </div>
-//     )
-// }
-// function ExperienceFrame({workplace, position, responsibilities, onChange, onClick, onClick2, idx}){
-   
-//     return (
-//         <div className="info-row">
-//           <div className = "idx">
-//             {idx}
-//           </div>
-//            <Input label = "Company name" value ={workplace} onChange={onChange}/>
-//            <Input label = "Position" value ={position} />
-//            <Input label = "Reponsibilities" value={responsibilities} />
-//             <button className="delete-button" onClick={onClick2}>📝</button>
-//             <button className="delete-button" onClick={onClick}>❌</button>
-//         </div>
-//     )
-// }
-
-
-function ExperienceFrame({workplace, position, responsibilities, onClick, onClick2, idx}){
-   
-    return (
-        <div className="info-row">
-          <div className = "idx">
-            {idx}
-          </div>
-            <div className="info-cell company-name">
-            <h4>Company Name: </h4>
-            <p>{workplace}</p>
-            </div>
-            <div className="info-cell job-title">
-            <h5>Job title: </h5>
-            <p>{position}</p>
-            </div>
-            <div className="info-cell responsibilities">
-            <h5>Responsibilities: </h5>
-            <p>{responsibilities}</p>
-            </div>
-            <button className="delete-button" onClick={onClick2}>📝</button>
-            <button className="delete-button" onClick={onClick}>❌</button>
+      <div className="experience-frame">
+        {experienceArray.length > 0
+          ? experienceArray.map((exp) => {
+              return (
+                <div className="inputs" key={exp.id}>
+                  <Input
+                    name={"Workplace" + (experienceArray.indexOf(exp) + 1)}
+                    type="text"
+                    label={
+                      "Workplace" + " " + (experienceArray.indexOf(exp) + 1)
+                    }
+                    value={exp.workplace}
+                    key={exp.id}
+                    onChange={(e) => handleEdit(e, exp.id, "workplace")}
+                  />{" "}
+                  <Input
+                    name="position"
+                    label="Position title"
+                    value={exp.position}
+                    onChange={(e) => handleEdit(e, exp.id, "position")}
+                  />{" "}
+                  <Input
+                    label="Responsibilities"
+                    value={exp.responsibilities}
+                    onChange={(e) => handleEdit(e, exp.id, "responsibilities")}
+                  />{" "}
+                  <Input
+                    label="Start date"
+                    type="month"
+                    value={exp.from}
+                    onChange={(e) => handleEdit(e, exp.id, "from")}
+                  />{" "}
+                  <Input
+                    label="End date"
+                     type="month"
+                    value={exp.to}
+                    onChange={(e) => handleEdit(e, exp.id, "to")}
+                  />{" "}
+                  <button onClick={() => handleDeleteClick(exp.id)}>x</button>
+                </div>
+              );
+            })
+          : null}
+      </div>
+      <div>
+        <div className="inputs">
+          <Input
+            name="workplace"
+            type="text"
+            label="Company name"
+            placeholder="Enter company"
+            value={experience.workplace}
+            onChange={(e)=>handleChange(e, "workplace")}
+          />{" "}
+          <Input
+            name="position"
+            label="Position title"
+            value={experience.position}
+            onChange={(e)=>handleChange(e, "position")}
+          />{" "}
+          <Input
+            label="Responsibilities"
+            placeholder="Enter responsibilities of your role"
+            value={experience.completed}
+            onChange={(e)=>handleChange(e, "completed")}
+          />{" "}
+          <Input
+            name="start-date"
+            type="month"
+            label="From"
+            value={experience.from}
+            onChange={(e)=>handleChange(e, "from")}
+          />
+          <Input
+            name="end-date"
+            type="month"
+            label="To"
+            value={experience.to}
+            onChange={(e)=>handleChange(e, "to")}
+          />
+          <button onClick={addExperience}>Add Experience</button>
         </div>
-    )
+      </div>
+      <div className="inputs"></div>
+    </div>
+  );
+}
+
+function Input({ label, placeholder, value, onChange, objKey, type, idx }) {
+  return (
+    <>
+      <div className="details-input-div" key={idx}>
+        <label htmlFor={name}>
+          {label}
+          {": "}
+
+          <input
+            key={idx}
+            type={type}
+            name={label}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+          />
+        </label>
+      </div>
+    </>
+  );
+}
+
+function ExperienceFrame({
+  workplace,
+  position,
+  responsibilities,
+  onClick,
+  onClick2,
+  idx,
+}) {
+  return (
+    <div className="info-row">
+      <div className="idx">{idx}</div>
+      <div className="info-cell company-name">
+        <h4>Company Name: </h4>
+        <p>{workplace}</p>
+      </div>
+      <div className="info-cell job-title">
+        <h5>Job title: </h5>
+        <p>{position}</p>
+      </div>
+      <div className="info-cell responsibilities">
+        <h5>Responsibilities: </h5>
+        <p>{responsibilities}</p>
+      </div>
+      <button className="delete-button" onClick={onClick2}>
+        📝
+      </button>
+      <button className="delete-button" onClick={onClick}>
+        ❌
+      </button>
+    </div>
+  );
 }
